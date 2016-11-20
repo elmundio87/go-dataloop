8b9c2b5ffecf93c2a053b864aca7c8f85f5cb8cb
elmundio87 <elmundio1987@gmail.com>
2016-11-20 10:26:14 +0000

    Implemented full spec of Account API

--- dataloop/accounts.go
+++ dataloop/accounts.go
@@ -1,9 +1,7 @@
 package dataloop
 
 import (
-
 	"net/http"
-
 	"github.com/dghubble/sling"
 )
 
@@ -37,6 +35,19 @@ type SuccessUpdate struct {
 	name  string `json:"name"`
 }
 
+type Account struct {
+    Id string `json:"id"`
+    Name string `json:"name"`
+    Agent_keys []string `json:"agent_keys"`
+}
+
+type AccountList []Account
+
+type AccountRoleSummary struct {
+    Account string `json:"account"`
+    Org string `json:"org"`
+}
+
 // Create a new account on Dataloop.
 func (o *AccountService) Create(accountName string) (*SuccessCreate, *http.Response, error) {
 
@@ -49,4 +60,70 @@ func (o *AccountService) Create(accountName string) (*SuccessCreate, *http.Respo
 	resp, err := o.sling.New().Post("accounts").BodyJSON(requestBody).Receive(success, nil)
 
 	return success, resp, err
+}
+
+// Update an existing account name on Dataloop.
+func (o *AccountService) Update(accountName string, newAccountName string) (*SuccessCreate, *http.Response, error) {
+
+	success := new(SuccessCreate)
+
+  requestBody := &AccountRequest{
+    Name: newAccountName,
+  }
+
+	resp, err := o.sling.New().Put("accounts/" + accountName ).BodyJSON(requestBody).Receive(success, nil)
+
+	return success, resp, err
+}
+
+// Update an existing account name on Dataloop.
+func (o *AccountService) Patch(accountName string, newAccountName string) (*SuccessCreate, *http.Response, error) {
+
+	success := new(SuccessCreate)
+
+  requestBody := &AccountRequest{
+    Name: newAccountName,
+  }
+
+	resp, err := o.sling.New().Patch("accounts/" + accountName ).BodyJSON(requestBody).Receive(success, nil)
+
+	return success, resp, err
+}
+
+// Retrieve an account list from Dataloop.
+func (o *AccountService) List() (*AccountList, *http.Response, error) {
+
+  var dataloopAccounts = new(AccountList)
+
+	resp, err := o.sling.New().Get("accounts").Receive(dataloopAccounts, nil)
+
+	return dataloopAccounts, resp, err
+}
+
+// Retrieve a single account from Dataloop.
+func (o *AccountService) Retrieve(accountName string) (*Account, *http.Response, error) {
+
+	var dataloopAccount = new(Account)
+
+	resp, err := o.sling.New().Get("accounts/" + accountName).Receive(dataloopAccount, nil)
+
+	return dataloopAccount, resp, err
+}
+
+// Retrieve an account role summary from Dataloop.
+func (o *AccountService) RoleSummary(accountName string) (*AccountRoleSummary, *http.Response, error) {
+
+	var dataloopAccountRoleSummary = new(AccountRoleSummary)
+
+	resp, err := o.sling.New().Get("accounts/" + accountName + "/features").Receive(dataloopAccountRoleSummary, nil)
+
+	return dataloopAccountRoleSummary, resp, err
+}
+
+// Delete an existing account from Dataloop.
+func (o *AccountService) Delete(accountName string) (*http.Response, error) {
+
+	resp, err := o.sling.New().Delete("accounts/" + accountName ).Receive(nil, nil)
+
+	return resp, err
 }
--- dataloop/dataloop.go
+++ dataloop/dataloop.go
@@ -18,7 +18,7 @@ type Client struct {
 // NewClient returns a new Client.
 func NewClient(httpClient *http.Client, dataloopOrg, dataloopRestApiKey string) *Client {
 
-	base := sling.New().Client(httpClient).Base(DataloopAPI + "/orgs/" + dataloopOrg + "/").Set("Authorization", "Bearer " + dataloopRestApiKey)
+	base := sling.New().Client(httpClient).Base(DataloopAPI + "/orgs/" + dataloopOrg + "/").Set("Authorization", "Bearer " + dataloopRestApiKey).Set("content-type", "multipart/form-data").Set("boundary", "---011000010111000001101001")
 
 	return &Client{
 		Accounts: NewAccountService(base.New()),
